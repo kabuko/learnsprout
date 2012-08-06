89928c44289da6b2fb1dcb41d574a24628ae3f5c
Thomas R. Koll <tomk32@tomk32.de>
2012-08-06 19:24:07 +0200

    Add Page#any? so we know when it is enough

--- lib/learnsprout/page.rb
+++ lib/learnsprout/page.rb
@@ -1,33 +1,43 @@
 module LearnSprout
-    class Page
-        include LearnSprout::Connection
+  class Page
+    include LearnSprout::Connection
 
-        attr_accessor :items
+    attr_accessor :items
 
-        def initialize(url, type, extras = {})
-            @items = []
-            @type = type
-            @extras = extras
-            data = get(url)
+    def initialize(url, type, extras = {})
+      return false if url.nil? || url == ""
+      @items = []
+      @type = type
+      @extras = extras
+      data = get(url)
 
-            @nextUrl = data["next"].to_s
-            # Remove base URL if it's included
-            prefix = LearnSprout.endpoint.to_s
-            if @nextUrl[0, prefix.length] == prefix
-                @nextUrl = @nextUrl[prefix.length, @nextUrl.length - prefix.length]
-            end
+      @nextUrl = data["next"].to_s
+      # Remove base URL if it's included
+      prefix = LearnSprout.endpoint.to_s
+      if @nextUrl[0, prefix.length] == prefix
+        @nextUrl = @nextUrl[prefix.length, @nextUrl.length - prefix.length]
+      end
 
-            #TODO handle non-page URL?
-            data["data"].each do |item|
-                extras.each do |key, value|
-                    item[key] = value
-                end
-                @items << type.new(item)
-            end
+      #TODO handle non-page URL?
+      data["data"].each do |item|
+        extras.each do |key, value|
+          item[key] = value
         end
-
-        def next
-            return Page.new(@nextUrl, @type, @extras)
-        end
+        @items << type.new(item)
+      end
     end
+
+
+    # page = api_client.schools(org_id)
+    # while page.any? do
+    #   ...
+    # end
+    def any?
+      @items && @items.any?
+    end
+
+    def next
+      return Page.new(@nextUrl, @type, @extras)
+    end
+  end
 end
